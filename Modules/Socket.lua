6caa46ace6b4cf624c55efe8223717ae10359ead
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-10-27 11:54:25 +0200

    change how modules are loaded and fix websocket

--- Modules/Socket.lua
+++ Modules/Socket.lua
@@ -1,33 +1,44 @@
-return function()
+return {
+    IsClient = true,
+    IsServer = false,
 
-    print("socket loaded")
+    Module = function()
 
-    -- Establish a new WSS connection to wss://echo.websocket.org:443
-    -- Which is an echo server, meaning, whatever you send will
-    -- get a response with the same contents you originally sent.
-    local Response, Read, Write = Webstocket.connect({
-        host = "ws.achex.ca",
-        port = 443,
-        tls = true
-    })
+        print("socket loaded")
     
-    -- Did the connection get established successfully? if not what's the error?
-    if not Response then
-        print("Error establishing connection!\n" .. Read); return
+        -- Establish a new WSS connection to wss://echo.websocket.org:443
+        -- Which is an echo server, meaning, whatever you send will
+        -- get a response with the same contents you originally sent.
+        local Response, Read, Write = Webstocket.connect({
+            host = Control.SocketHost,
+            pathname = Control.SocketPath,
+            port = Control.SocketPort,
+        })
+        
+        -- Did the connection get established successfully? if not what's the error?
+        if not Response then
+            print("Error establishing connection! (" .. Read .. ")"); return
+        end
+
+        p(Response)
+        
+        -- Send a message opcode 2 with the payload of "Hello There!"
+        Write({
+            payload = "Hello There!"
+        })
+        
+        Write({
+            payload = "How are ya"
+        })
+
+        p("a ea raaera")
+    
+        for Message in Read do
+            print("Host responded with " .. Message.payload)
+        end
+        print("Connection closed!")
+    
     end
-    
-    -- Send a message opcode 2 with the payload of "Hello There!"
-    Write({
-        payload = "Hello There!"
-    })
-    
-    Write({
-        payload = "How are ya"
-    })
+}
 
-    for Message in Read do
-        print("Host responded with " .. Message.payload)
-    end
-    print("Connection closed!")
 
-end
--- Modules/SugarBowl.lua
+++ Modules/SugarBowl.lua
@@ -1,8 +1,33 @@
-return function()
-    local App = WebLit.app
+return {
+    IsClient = false,
+    IsServer = true,
 
-    .bind({
-        host = "127.0.0.1",
-        port = 8080
-    })
-end
+    Module = function()
+        local App = WebLit.app
+    
+        App.bind({
+            host = Control.SocketHost,
+            port = Control.SocketPort
+        })
+
+        App.use(WebLit.logger)
+
+        App.websocket(
+            {
+                path = Control.SocketPath, -- Prefix for matching
+            },
+            function (Request, Read, Write)
+                -- Log the request headers
+                p(Request)
+                -- Log and echo all messages
+                for Message in Read do
+                    Write(Message)
+                end
+                -- End the stream
+                Write()
+            end
+        )
+
+        App.start()
+    end
+}
