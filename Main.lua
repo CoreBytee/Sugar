3829aea77b95b3c99d968080c06cc0e024a410bb
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-10-27 11:56:05 +0200

    change the coroutines

--- Main.lua
+++ Main.lua
@@ -1,19 +1,31 @@
-
-Logger.Info(" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ")
-Logger.Info("█ ▄▄▄▄█▄ ██ ▄█ ▄▄▄▄██▀▄ ██▄ ▄▄▀█")
-Logger.Info("█▄▄▄▄ ██ ██ ██ ██▄ ██ ▀ ███ ▄ ▄█")
-Logger.Info("█▄▄▄▄▄██▄▄▄▄██▄▄▄▄▄█▄▄█▄▄█▄▄█▄▄█")
-Logger.Info("████████████████████████████████")
-Logger.Info(string.format("█████▀▀▀ %s Edition ▀▀▀█████", RunTypeString))
-Logger.Info("█████ MADE BY : [REDACTED] █████")
-Logger.Info("▀██████████████████████████████▀")
-
-
-
+return function()
+    Logger.Info(" ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ")
+    Logger.Info("████████████████████████████████")
+    Logger.Info("█ ▄▄▄▄█▄ ██ ▄█ ▄▄▄▄██▀▄ ██▄ ▄▄▀█")
+    Logger.Info("█▄▄▄▄ ██ ██ ██ ██▄ ██ ▀ ███ ▄ ▄█")
+    Logger.Info("█▄▄▄▄▄██▄▄▄▄██▄▄▄▄▄█▄▄█▄▄█▄▄█▄▄█")
+    Logger.Info("████████████████████████████████")
+    Logger.Info(string.format("█████▀▀▀ %s Edition ▀▀▀█████", RunTypeString))
+    Logger.Info("█████ MADE BY : [REDACTED] █████")
+    Logger.Info("▀██████████████████████████████▀")
 
 
-for i, v in pairs(FS.readdirSync("./Modules/")) do
-    coroutine.wrap(function()
-        require("./Modules/" .. v)()
-    end)()
+
+
+
+    for i, v in pairs(FS.readdirSync("./Modules/")) do
+        
+        local Module = require("./Modules/" .. v)
+
+        if RunType == true then
+            if Module.IsClient == true then
+                coroutine.wrap(Module.Module)()
+            end
+        else
+            if Module.IsServer == true then
+                coroutine.wrap(Module.Module)()
+            end
+        end
+
+    end
 end
