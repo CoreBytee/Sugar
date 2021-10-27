f90aeb1acf480e20ebb626e26103f73fc611f6d2
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-10-27 21:20:02 +0200

    Create CommandHandler.lua

---
+++ Libs/CommandHandler.lua
@@ -0,0 +1,21 @@
+_G.Commands = {}
+
+local Library = {}
+
+Library.AddCommand = function(Name, CommandFunction)
+    Commands[Name] = {Run = CommandFunction}
+end
+
+Library.HandleCommand = function(Data)
+    Commands[Data.Name].Run(Data.Params)
+end
+
+Library.HandleRawData = function(Message)
+    Library.HandleCommand(Json.parse(Message.payload))
+end
+
+Library.RunCommand = function(Name, Params)
+    return Json.encode({Name = Name, Params = Params})
+end
+
+return Library
