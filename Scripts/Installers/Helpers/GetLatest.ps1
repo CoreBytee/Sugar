ed12c2725fe0e4487a5956116d6975f048e67a15
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-10-28 15:28:17 +0200

    Create GetLatest.ps1

---
+++ Scripts/Installers/Helpers/GetLatest.ps1
@@ -0,0 +1,8 @@
+$repo = "CoreBytee/Sugar"
+
+$releases = "https://api.github.com/repos/$repo/releases"
+
+Write-Host Determining latest release
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+
+return $tag
