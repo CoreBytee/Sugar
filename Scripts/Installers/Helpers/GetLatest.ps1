3ff8b5f6844c8e2c178c32708fdbe49d8364a818
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-10-28 19:17:44 +0200

    sigh

--- Scripts/Installers/Helpers/GetLatest.ps1
+++ Scripts/Installers/Helpers/GetLatest.ps1
@@ -3,6 +3,6 @@ $repo = "CoreBytee/Sugar"
 $releases = "https://api.github.com/repos/$repo/releases"
 
 Write-Host Determining latest release
-$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+$tag = (Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
 
 return $tag
