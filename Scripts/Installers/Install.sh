a0db9a1d1c1939dc416eddee7a87c15110c1ef8e
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-10-29 10:38:56 +0200

    yay i made a installer

--- Scripts/Installers/Install.sh
+++ Scripts/Installers/Install.sh
@@ -1,1 +1,43 @@
-echo aaa
+if [ -d "./Sugar" ]
+then
+    echo "Removing old version in 3 seconds."
+    sleep 1
+    rm -rf "./Sugar"
+    sleep 1
+fi
+
+tag=$(curl -L https://raw.githubusercontent.com/CoreBytee/Sugar/main/Scripts/Installers/Helpers/GetLatest.sh | sh)
+
+echo "Tag is $tag"
+
+curl -o Installer.zip -L "https://github.com/CoreBytee/Sugar/archive/refs/tags/$tag.zip"
+
+unzip Installer -d ./
+
+rm ./Installer.zip
+
+mv "Sugar-$tag" "Sugar"
+
+mkdir ./Sugar/Deps/
+mkdir ./Sugar/Binary/
+mkdir ./Sugar/Binary/Luvit/
+mkdir ./Sugar/Binary/FFTools/
+
+curl -o Binary.zip -L "https://github.com/CoreBytee/Sugar/releases/download/0.0.1/Binary.zip"
+unzip Binary -d ./Binary
+
+mv ./Binary/luvit ./Sugar/Binary/Luvit/
+mv ./Binary/lit ./Sugar/Binary/Luvit/
+mv ./Binary/ffplay ./Sugar/Binary/FFTools/
+mv ./Binary/brightness ./Sugar/Binary/
+
+rm -rf ./Binary/
+rm ./Binary.zip
+
+cd Sugar
+
+./Binary/Luvit/lit install
+
+echo "return false" > "./Config/DevMode.lua"
+
+./Binary/Luvit/luvit ./Init.lua
