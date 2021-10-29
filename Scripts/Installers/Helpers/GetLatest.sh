3650b716f927aa05c56b72fdfc46705448d73bc3
ScriptItWithCode <62032154+CoreBytee@users.noreply.github.com>
2021-10-29 09:00:25 +0200

    E

--- Scripts/Installers/Helpers/GetLatest.sh
+++ Scripts/Installers/Helpers/GetLatest.sh
@@ -1,8 +1,4 @@
 repo="CoreBytee/Sugar"
 
-echo "$repo"
-
-
 curl -s "https://api.github.com/repos/$repo/releases" | \
     python3 -c "import sys, json; print(json.load(sys.stdin)[0]['tag_name'])"
-
