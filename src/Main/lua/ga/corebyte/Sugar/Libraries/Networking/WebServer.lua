68618063902e6afe76bf8ad365684113202f4f86
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-02 13:40:58 +0200

    more base networking

---
+++ src/Main/lua/ga/corebyte/Sugar/Libraries/Networking/WebServer.lua
@@ -0,0 +1,50 @@
+local WebServer = Object:extend()
+
+local SocketServer = Import("ga.corebyte.Sugar.Libraries.Networking.SocketServer")
+
+function WebServer:initialize()
+    local App = require("weblit").app
+
+    App.bind(
+        {
+            host = "0.0.0.0",
+            port = Sugar.Config:GetOption("SugarBowl.Port")
+        }
+    )
+    
+    App.use(require('weblit-logger'))
+    App.use(require('weblit-auto-headers'))
+    
+    self.App = App
+end
+
+function WebServer:Start()
+    self.App.start()
+end
+
+function WebServer:Route(Options, Function)
+    self.App.route(
+        Options,
+        Function
+    )
+    return App
+end
+
+function WebServer:Socket(Options)
+    local SocketServer = SocketServer:new()
+    self.App.websocket(
+        Options,
+        function (Request, Read, Write)
+            SocketServer:NewConnection(
+                {
+                    Request = Request,
+                    Read = Read,
+                    Write = Write
+                }
+            )
+        end
+    )
+    return SocketServer
+end
+
+return WebServer
