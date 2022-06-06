2d7eb72fc5ef24591efca2c05ea2a35620d1ba82
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-06 14:15:31 +0200

    more fixing services

--- src/Main/lua/ga/corebyte/Sugar/Registry/ServiceRegistry.lua
+++ src/Main/lua/ga/corebyte/Sugar/Registry/ServiceRegistry.lua
@@ -1,23 +1,31 @@
 --#region Services
 local Services = {
-    Banner = Import("ga.corebyte.Sugar.Services.Banner"),
-    NetworkClient = Import("ga.corebyte.Sugar.Services.Networking.Client"),
-    ClientHandshake = Import("ga.corebyte.Sugar.Services.Networking.ClientHandshake"),
-    NetworkServer = Import("ga.corebyte.Sugar.Services.Networking.Server"),
-    EchoCommand = Import("ga.corebyte.Sugar.Services.Commands.Echo")
+    Client = {
+        Banner = Import("ga.corebyte.Sugar.Services.Banner"),
+        Binary = Import("ga.corebyte.Sugar.Services.Binary"),
+        NetworkClient = Import("ga.corebyte.Sugar.Services.Networking.Client.Client"),
+        ClientHandshake = Import("ga.corebyte.Sugar.Services.Networking.Client.ClientHandshake"),
+        EchoCommand = Import("ga.corebyte.Sugar.Services.Commands.Echo")
+    },
+    Server = {
+        Banner = Import("ga.corebyte.Sugar.Services.Banner"),
+        NetworkServer = Import("ga.corebyte.Sugar.Services.Networking.Server.Server"),
+        ServerHandshake = Import("ga.corebyte.Sugar.Services.Networking.Server.ServerHandshake")
+    }
 }
 --#endregion
 return function (ServiceManager)
 --#region Register Client Services
-    ServiceManager:RegisterClientService(Services.Banner)
-    ServiceManager:RegisterClientService(Services.NetworkClient, true)
-    ServiceManager:RegisterClientService(Services.ClientHandshake)
-    ServiceManager:RegisterClientService(Services.EchoCommand)
+    ServiceManager:RegisterClientService(Services.Client.Banner)
+    ServiceManager:RegisterClientService(Services.Client.Binary, true)
+    ServiceManager:RegisterClientService(Services.Client.NetworkClient, true)
+    ServiceManager:RegisterClientService(Services.Client.ClientHandshake)
+    ServiceManager:RegisterClientService(Services.Client.EchoCommand)
 --#endregion
 --#region Register Server Services
-    ServiceManager:RegisterServerService(Services.Banner)
-    ServiceManager:RegisterServerService(Services.NetworkServer, true)
-
+    ServiceManager:RegisterServerService(Services.Server.Banner)
+    ServiceManager:RegisterServerService(Services.Server.NetworkServer, true)
+    ServiceManager:RegisterServerService(Services.Server.ServerHandshake)
 --#endregion
     return ServiceManager
 end
